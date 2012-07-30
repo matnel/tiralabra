508838970397da3626d8a87b45e2556932d84673
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-07-30 22:49:07 +0300

    Method stubs for testing.

--- src/model/Graph.java
+++ src/model/Graph.java
@@ -1,8 +1,77 @@
 package model;
 
+import java.util.*;
+
 /**
- * Represents one graph including all nodes in the graph. 
- ***/
+ * Represents one graph including all nodes in the graph.
+ * 
+ * @version alpha
+ * @author Matti Nelimarkka
+ **/
 public class Graph {
+	
+	private List<Node> edges;
+	
+	/**
+	 * Default constructor, takes no parameters and sets the graph as empty.
+	 **/
+	public Graph() {
+		// TODO: change to my own list implementation
+		edges = new ArrayList<Node>();
+	}
+	
+
+	/**
+	 * Adds a new node to the graph.
+	 * Confirms, that there is no node with same name before doing the addition.
+	 * 
+	 * @return true if successful
+	 * */
+	public boolean addNode(Node n) {
+		return true;
+	}
+	
+	/** 
+	 * Returns the number of nodes in this graph.
+	 * 
+	 * @return number of nodes
+	 **/
+	public int size() {
+		return -1;
+	}
+	
+	/**
+	 * Returns the graph in a matrix format, by starting to cound from the first element in the node list.
+	 **/
+	public int[][] matrix() {
+		return null;
+	}
+	
+	/**
+	 * Checks if n is part of this graph.
+	 * 
+	 * @retun true if n is part of the graph and f otherwise
+	 * */
+	public boolean has(Node n) {
+		return false;
+	}
+
+	/**
+	 * Checks if node with name name is part of this graph.
+	 * 
+	 * @retun true if n is part of the graph and f otherwise
+	 * */
+	public boolean has(String name) {
+		return false;
+	}
+	
+	/**
+	 * Returns the edges of this grap.
+	 * **/
+	public List<Node> edges() {
+		return null;
+	}
+	
+	// abstract Map shortestPaths();
 
 }
--- src/model/Node.java
+++ src/model/Node.java
@@ -1,8 +1,79 @@
 package model;
 
+import java.util.*;
+
 /**
  * Represents one edge in the graph.
- * **/
+ * 
+ * @version alpha
+ * @author Matti Nelimarkka
+ **/
 public class Node {
+	
+	private Map<Node,Double> neighbor;
+	private String name;
+	
+	/**
+	 * Default constructor, creates an single node.
+	 * 
+	 * @param name name of this node
+	 * */
+	public Node(String name) {
+		// FIXME: use own implementation
+		neighbor = new HashMap<Node,Double>();
+	}
+
+	/**
+	 * Constructor, creates a list with already linked connections.
+	 * 
+	 * @param name name of this node
+	 * @param neighbors weighted list of connections
+	 * */
+	public Node(String name, Map<Node, Double> neighbors) {
+		this(name);
+	}
+	
+	/**
+	 * Set n connected this node with the weight weight.
+	 * 
+	 * If the nodes were already connected, returns true and updates the weight, otherwise returns false.
+	 * 
+	 * @param n the node where connection is made
+	 * @param weight weight of the connection
+	 * @return true if the nodes were already connected, false otherwise
+	 **/
+	public boolean linkTo(Node n, double weight) {
+		return true;
+	}
+	
+	
+	/**
+	 * Checks if this node and n are connected, i.e. if n is node's neighbor.
+	 * 
+	 * @param n the node tested
+	 * @return true if n is node's neighbor
+	 **/
+	public boolean linkTo(Node n) {
+		return false;
+	}
+	
+	/**
+	 * Provides the weight from this node to n.
+	 * 
+	 * @param n the node tested
+	 * @return the link weight or -1
+	 * */
+	public double linkWeight(Node n) {
+		return -100;
+	}
+	
+	/**
+	 * Returns node's neighbors.
+	 * 
+	 * @return map of node's neigbbors and the weights.
+	 * */
+	public Map<Node,Double> neighbors() {
+		return null;
+	}
 
 }
