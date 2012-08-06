d0620b32cceddee9fc828535c53e731b951b60e3
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-08-06 22:22:45 +0300

    Skeleton tests

--- src/test/MyListTest.java
+++ src/test/MyListTest.java
@@ -2,12 +2,132 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyListTest {
 
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	@Test
-	public void test() {
+	public void testAddE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddIntE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAllCollectionOfQextendsE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAllIntCollectionOfQextendsE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIndexOf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLastIndexOf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListIteratorInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetainAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToArrayTArray() {
 		fail("Not yet implemented");
 	}
 
