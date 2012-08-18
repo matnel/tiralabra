02635f6310a836604faf03b11c5e1ad024280485
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-08-19 00:47:26 +0300

    Tests for critical classes

--- src/test/MyMapTest.java
+++ src/test/MyMapTest.java
@@ -6,10 +6,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import datastructures.MyMap;
+
 public class MyMapTest {
+	
+	private MyMap<String, String> map;
 
 	@Before
 	public void setUp() throws Exception {
+		map = new MyMap<String, String>();
 	}
 
 	@After
@@ -18,17 +23,28 @@ public class MyMapTest {
 
 	@Test
 	public void testClear() {
-		fail("Not yet implemented");
+		map.put("A", "A");
+		assertFalse("Map should not be empty", map.isEmpty() );
+		map.clear();
+		assertTrue("Map should be empty", map.isEmpty() );
 	}
 
 	@Test
 	public void testContainsKey() {
-		fail("Not yet implemented");
+		map.put("a", "a");
+		map.put("A", "A");
+		assertFalse("Should not have B", map.containsKey("B") );
+		assertTrue("Should have a", map.containsKey("a") );
+		assertTrue("Should have A", map.containsKey("A") );
 	}
 
 	@Test
 	public void testContainsValue() {
-		fail("Not yet implemented");
+		map.put("a", "a");
+		map.put("A", "A");
+		assertFalse("Should not have B", map.containsValue("B") );
+		assertTrue("Should have a", map.containsValue("a") );
+		assertTrue("Should have A", map.containsValue("A") );
 	}
 
 	@Test
@@ -38,12 +54,23 @@ public class MyMapTest {
 
 	@Test
 	public void testGet() {
-		fail("Not yet implemented");
+		map.put("a", "a");
+		map.put("A", "A");
+		assertNull("Null must be returned for invalid key", map.get("B") );
+		assertEquals("a should be in map", "a", map.get("a") );
+		assertEquals("A should be in map", "A", map.get("A") );
 	}
 
 	@Test
 	public void testIsEmpty() {
-		fail("Not yet implemented");
+		assertTrue( "Map should me empty", map.isEmpty() );
+		map.put("A", "A");
+		map.put("B", "B");
+		assertFalse("Map should not be empty", map.isEmpty() );
+		map.remove("B");
+		assertFalse("Map should not be empty", map.isEmpty() );
+		map.clear();
+		assertTrue("Map should be empty", map.isEmpty() );
 	}
 
 	@Test
@@ -53,7 +80,22 @@ public class MyMapTest {
 
 	@Test
 	public void testPut() {
-		fail("Not yet implemented");
+		assertNull("No value has been stored previously", map.put("Matti", "Meikäläinen") );
+		assertEquals("Meikäläinen has been stored", "Meikäläinen", map.get("Matti") );
+		assertEquals("Meikäläinen was stored previously", "Meikäläinen", map.put("Matti", "Mehiläinen") );
+		assertEquals("Mehiläinen has been stored", "Mehiläinen", map.get("Matti") );
+		
+		// store in the same bucket chain
+		assertNull("No value has been stored previously", map.put("3","3") );
+		assertEquals("3 has been stored", "3", map.get("3") );
+		
+		// couse a rehash
+		for(int i = -10; i < 10; i++) {
+			String t = "" + i;
+			map.put( t, t );
+			assertEquals("Missing value for " + t, t, map.get(t) );
+		}
+		assertEquals("Missing value Mehiläinen", "Mehiläinen", map.get("Matti") );
 	}
 
 	@Test
@@ -68,7 +110,15 @@ public class MyMapTest {
 
 	@Test
 	public void testSize() {
-		fail("Not yet implemented");
+		assertEquals( "Map should be empty", 0, map.size() );
+		map.put("A", "A");
+		assertEquals( "Map should have 1 element", 1, map.size() );
+		map.put("B", "B");
+		assertEquals( "Map should have 2 element", 2, map.size() );
+		map.remove("B");
+		assertEquals( "Map should have 1 element", 1, map.size() );
+		map.clear();
+		assertEquals( "Map should be empty", 0, map.size() );
 	}
 
 	@Test
