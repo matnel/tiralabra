ef21d3d693b819f8ffdbed647f7393c70167996f
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-08-19 13:30:35 +0300

    Implementation classes for both Dikstra's and A* algorithms.

---
+++ src/model/DikstraPathGraph.java
@@ -0,0 +1,20 @@
+package model;
+import java.util.List;
+
+
+
+public class DikstraPathGraph extends ShortestPathGraph {
+
+	@Override
+	public double distance(Node from, Node to) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Node> path(Node from, Node to) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
