158e0a5b42aea7b4f0601c8b752bac374f6bcd64
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-08-19 13:12:56 +0300

    Tests for set

--- src/test/MySetTest.java
+++ src/test/MySetTest.java
@@ -2,33 +2,55 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import datastructures.MySet;
+
 public class MySetTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	
+	MySet<Integer> set = new MySet<Integer>();
 
 	@Test
 	public void testAddE() {
-		fail("Not yet implemented");
+		// fill the set up
+		for(int i = 0; i < 10; i++) {
+			assertTrue( "Adding unique element to set failed", set.add(i) );
+		}
+		assertFalse( "Adding duplicate elements to set success", set.add(0) );
+		assertEquals("There should be only one 0", 0, set.lastIndexOf(0) );
 	}
 
-	@Test
+	@Test(expected=UnsupportedOperationException.class)
 	public void testAddIntE() {
-		fail("Not yet implemented");
+		set.add(0, 10);
+	}
+	
+	@Test(expected=UnsupportedOperationException.class)
+	public void testAddAllIntECollectionOfQextendsE() {
+		Collection<Integer> c = new ArrayList<Integer>();
+		c.add(10);
+		c.add(0);
+		set.addAll(0, c);
 	}
 
 	@Test
 	public void testAddAllCollectionOfQextendsE() {
-		fail("Not yet implemented");
+		set.add(0);
+		
+		Collection<Integer> c = new ArrayList<Integer>();
+		c.add(10);
+		c.add(0);
+		assertTrue("Adding unique elements should be succesfull", set.addAll(c) );
+		assertEquals("0 is in correct position", 0, set.lastIndexOf(0) );
+		assertEquals("10 is in correct place", 1, set.lastIndexOf(10) );
+		assertEquals("The set is correct size", 2, set.size() );
+		assertFalse( "Adding duplicate elements should not be succesfull", set.addAll(c) );
 	}
 
 }
