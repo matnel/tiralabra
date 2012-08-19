10bee5052b29dd40cdcfca483774251cbcb1fa46
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-08-19 12:36:47 +0300

    Skeleton class

---
+++ src/datastructures/MyIterator.java
@@ -0,0 +1,66 @@
+package datastructures;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class MyIterator<E> implements ListIterator<E> {
+	
+	public MyIterator(List <E> list) {
+		
+	}
+
+	@Override
+	public void add(E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int nextIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E previous() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int previousIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set(E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
