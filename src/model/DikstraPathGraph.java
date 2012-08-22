ea9a6742eccf0c2f26f31a0c44909085193389b6
Matti <mnelimar@chatillon.pc.hiit.fi>
2012-08-22 22:47:50 +0300

    Initial thinking on distance() function

--- src/model/DikstraPathGraph.java
+++ src/model/DikstraPathGraph.java
@@ -1,5 +1,8 @@
 package model;
 import java.util.List;
+import java.util.Map;
+
+import datastructures.MyMap;
 
 
 
@@ -7,8 +10,43 @@ public class DikstraPathGraph extends ShortestPathGraph {
 
 	@Override
 	public double distance(Node from, Node to) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		// initialize all nodes and distances
+		int graphSize = size();
+		Map<Node,Double> distances = new MyMap<Node, Double>();
+		
+		for( Node n : edges() ) {
+			distances.put( n, - Double.MAX_VALUE );
+		}
+		
+		// set current node is zero distance
+		distances.put(from, 0.0 );
+		
+		List<Node> check = edges();
+		Node current = from;
+		check.remove( current );
+		while( ! check.isEmpty() ) {
+			
+			double currentDistance = distances.get(current);
+			for( Map.Entry<Node, Double> neigbour : current.neighbors().entrySet() ) {
+				Node n = neigbour.getKey();
+				double d = neigbour.getValue();
+				double candidate = currentDistance + d;
+				
+				// we have a shorter path! update
+				if( candidate < distances.get( n ) ) {
+					distances.put( n , candidate);
+				}
+				
+				// special case, there is no previous path to n, update!
+				if( distances.get( n ) == - Double.MAX_VALUE ) {
+					distances.put( n, candidate );
+				}
+			}
+			// take new node to be checked
+		}
+ 		
+		return distances.get( to );
 	}
 
 	@Override
