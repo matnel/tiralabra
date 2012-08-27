77475470a16786d57907d9bf6350f2ce7eccc3c6
Matti Nelimarkka <mnelimar@chatillon.pc.hiit.fi>
2012-08-27 21:24:13 +0300

    Initial drafts

--- src/model/DikstraPathGraph.java
+++ src/model/DikstraPathGraph.java
@@ -1,49 +1,62 @@
 package model;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import datastructures.MyMap;
-
-
+import datastructures.*;
 
 public class DikstraPathGraph extends ShortestPathGraph {
 
 	@Override
 	public double distance(Node from, Node to) {
-		
+
 		// initialize all nodes and distances
-		int graphSize = size();
-		Map<Node,Double> distances = new MyMap<Node, Double>();
+		Map<Node,Double> distances = new HashMap<Node, Double>();
+		
+		
+		PriorityQueue<QueueNode> check = new PriorityQueue<QueueNode>();
 		
 		for( Node n : edges() ) {
-			distances.put( n, INFINITY );
+			distances.put( n , INFINITY );
+			check.add( new QueueNode( n , INFINITY )  );
 		}
 		
 		// set current node is zero distance
-		distances.put(from, 0.0 );
+		distances.put( from, 0.0 );
 		
-		List<Node> check = edges();
+		
 		Node current = from;
-		check.remove( current );
+		QueueNode qCurrent = null;
 		while( ! check.isEmpty() ) {
-			
 			double currentDistance = distances.get(current);
 			for( Map.Entry<Node, Double> neigbour : current.neighbors().entrySet() ) {
 				Node n = neigbour.getKey();
 				double d = neigbour.getValue();
 				double candidate = currentDistance + d;
+				// System.exit(-1);
+				
+				// System.out.println( n.name() );
 				
 				// we have a shorter path! update
 				if( candidate < distances.get( n ) ) {
 					distances.put( n , candidate);
+					
+					// remove from que && update back with new value
+					check.remove( new QueueNode( n , -1 ) );
+					check.add( new QueueNode(n, candidate) );
 				}
 				
 				// special case, there is no previous path to n, update!
 				if( distances.get( n ) == INFINITY ) {
 					distances.put( n, candidate );
+					
+					// remove from que && update back
+					check.remove( new QueueNode( n , -1 ) );
+					check.add( new QueueNode(n, candidate) );
+					// System.out.println( check.size() );
+					
 				}
 			}
 			// take new node to be checked
+			current = check.poll().node();
 		}
  		
 		return distances.get( to );
@@ -52,7 +65,7 @@ public class DikstraPathGraph extends ShortestPathGraph {
 	@Override
 	public List<Node> path(Node from, Node to) {
 		// TODO Auto-generated method stub
-		return null;
+		return new MyList<Node>();
 	}
 
 }
