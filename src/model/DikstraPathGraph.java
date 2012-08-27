61ea512503d624d1c63f620a542a509e22933f9c
Matti Nelimarkka <mnelimar@chatillon.pc.hiit.fi>
2012-08-27 23:32:24 +0300

    Code cleanup

--- src/model/DikstraPathGraph.java
+++ src/model/DikstraPathGraph.java
@@ -8,7 +8,11 @@ public class DikstraPathGraph extends ShortestPathGraph {
 	@Override
 	public double distance(Node from, Node to) {
 		
+		// this is the most computation heavy operation
 		List<Node> path = path(from, to);
+		
+		// compared to generating the Dikstra's graph these are trivial,
+		// no major impact in O(n) sense!
 		
 		// special case, there's no path!
 		if( path.isEmpty() ) {
@@ -26,67 +30,16 @@ public class DikstraPathGraph extends ShortestPathGraph {
 		}
 		
 		return distance;
-
-		/*
-		// initialize all nodes and distances
-		Map<Node,Double> distances = new HashMap<Node, Double>();
-		
-		
-		PriorityQueue<QueueNode> check = new PriorityQueue<QueueNode>();
-		
-		for( Node n : edges() ) {
-			distances.put( n , INFINITY );
-			check.add( new QueueNode( n , INFINITY )  );
-		}
-		
-		// set current node is zero distance
-		distances.put( from, 0.0 );
-		
-		Node current = from;
-		while( ! check.isEmpty() ) {
-			double currentDistance = distances.get(current);
-			for( Map.Entry<Node, Double> neigbour : current.neighbors().entrySet() ) {
-				Node n = neigbour.getKey();
-				double d = neigbour.getValue();
-				double candidate = currentDistance + d;
-				
-				// special case, there is no previous path to n, update!
-				if( distances.get( n ) == INFINITY ) {
-					distances.put( n, candidate );
-					
-					// remove from que && update back
-					check.remove( new QueueNode( n , -1 ) );
-					check.add( new QueueNode(n, candidate) );
-					// System.out.println( check.size() );
-					
-				}
-				
-				// we have a shorter path! update
-				if( candidate < distances.get( n ) ) {
-					distances.put( n , candidate);
-					
-					// remove from que && update back with new value
-					check.remove( new QueueNode( n , -1 ) );
-					check.add( new QueueNode(n, candidate) );
-				}
-				
-			}
-			// take new node to be checked
-			current = check.poll().node();
-		}
- 		
-		return distances.get( to );
-		*/
 	}
 
 	@Override
 	public List<Node> path(Node from, Node to) {
+		// list of items that must be checked
+		PriorityQueue<QueueNode> check = new PriorityQueue<QueueNode>();
+		
 		// initialize all nodes and distances
 		Map<Node,Double> distances = new HashMap<Node, Double>();
 		Map<Node,List<Node>> paths = new HashMap<Node, List<Node>>();
-		
-		
-		PriorityQueue<QueueNode> check = new PriorityQueue<QueueNode>();
 		
 		for( Node n : edges() ) {
 			distances.put( n , INFINITY );
@@ -98,53 +51,48 @@ public class DikstraPathGraph extends ShortestPathGraph {
 		distances.put( from, 0.0 );
 		paths.get( from ).add( from );
 		
-		
-		
 		Node current = from;
+		
 		while( ! check.isEmpty() ) {
+			
 			double currentDistance = distances.get(current);
 			List<Node> currentPath = paths.get(current);
+			
 			for( Map.Entry<Node, Double> neigbour : current.neighbors().entrySet() ) {
+				
 				Node n = neigbour.getKey();
 				double d = neigbour.getValue();
 				double candidate = currentDistance + d;
 				
+				boolean update = false;
+				
 				// special case, there is no previous path to n, update!
 				if( distances.get( n ) == INFINITY ) {
-					distances.put( n, candidate );
-					
-					List<Node> newPath = new MyList<Node>();
-					newPath.addAll( currentPath );
-					newPath.add( n );
-					paths.put( n , newPath );
-					
-					// remove from que && update back
-					check.remove( new QueueNode( n , -1 ) );
-					check.add( new QueueNode(n, candidate) );
-					// System.out.println( check.size() );
-					
+					update = true;
 				}
 				
 				// we have a shorter path! update
-				if( candidate < distances.get( n ) ) {
+				if( candidate < distances.get( n ) || update ) {
+					// set new distance to candidate
 					distances.put( n, candidate );
 					
+					// set new path
 					List<Node> newPath = new MyList<Node>();
 					newPath.addAll( currentPath );
 					newPath.add( n );
 					paths.put( n , newPath );
 					
-					// remove from que && update back
+					// remove from que && update back to new position
 					check.remove( new QueueNode( n , -1 ) );
 					check.add( new QueueNode(n, candidate) );
 				}
 				
 			}
+			
 			// take new node to be checked
 			current = check.poll().node();
 		}
  		
-		System.out.println( paths.get( to ).size() );
 		return paths.get( to );
 	}
 
