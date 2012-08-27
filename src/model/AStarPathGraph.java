57dcc28a439b6335dbdcdb55f249658653ccc358
Matti Nelimarkka <mnelimar@chatillon.pc.hiit.fi>
2012-08-27 21:35:00 +0300

    Added an heuristic funtion that can be used

--- src/model/AStarPathGraph.java
+++ src/model/AStarPathGraph.java
@@ -3,6 +3,18 @@ package model;
 import java.util.List;
 
 public class AStarPathGraph extends ShortestPathGraph {
+	
+	/**
+	 * Provides an heuristic estimation of the path length between from and to.
+	 * 
+	 * @param a the node from the estimation is started
+	 * @param b the node where the estimation is done
+	 * 
+	 * @return the estimated value of the path length.
+	 * */
+	private double estimate(Node a, Node b) {
+		return -1;
+	}
 
 	@Override
 	public double distance(Node from, Node to) {
